2f13c70e64a5631e5b2da8f51eecbfaee529cf9a
Ultraviolet-N7 <meieramberj@gmail.com>
2021-01-05 22:21:22 -0600

    Finished ch. 2 studio + bonus.

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -5,17 +5,33 @@ import java.util.Scanner;
 public class Area {
 
     public static void main(String[] args) {
+        Area area = new Area();
+        area.calculateArea();
+
+    }
+
+    public void calculateArea() {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the radius of the circle: ");
-        double radius = input.nextDouble();
+        System.out.println("Please enter the radius of the circle:  ");
+        try {
+            boolean keepLooping = true;
+            while(keepLooping) {
+                Double radius = input.nextDouble();
 
-//        if (radius < 0 || radius == " " || ) {
-//            System.out.println("This is not a valid number fool!");
-//        }
+                if (radius < 0) {
+                    System.out.println("Input must be a positive number. Please try again: ");
+                } else {
+                    keepLooping = false;
+                    Double area = Circle.getArea(radius);
+                    System.out.println("The area of a circle of radius " + radius + " is: " + area);
+                }
+            }
+            } catch(Exception error) {
+            System.out.println("Input must be numeric.");
+        }
 
-        Double area = Circle.getArea(radius);
-        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+        input.close();
 
     }
 }
--- src/org/launchcode/java/studios/areaofacircle/Circle.java
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -2,6 +2,8 @@ package org.launchcode.java.studios.areaofacircle;
 
 public class Circle {
 
+    public Circle() { }
+
     public static Double getArea(Double radius) {
         return 3.14 * radius * radius;
     }
